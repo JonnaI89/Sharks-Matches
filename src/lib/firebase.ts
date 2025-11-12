e73b28b279bf2e8f53dd178eed08c0a76b097b55
Jonas Ingebretsen <JIngebretsen89@gmail.com>
2025-11-12 07:53:46 +0000

    Den er blitt borte. Vi må bruke // Import the functions you need from th

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAP71GFs3rAbCI92ukHcWI0-Y-qN956vDg",
-  authDomain: "internview-c36e2.firebaseapp.com",
-  projectId: "internview-c36e2",
-  storageBucket: "internview-c36e2.appspot.com",
-  messagingSenderId: "399554552443",
-  appId: "1:399554552443:web:c118ed9f379df98520b8de",
-  measurementId: "G-N24VL18VFR"
+  apiKey: "AIzaSyAwLTr3MJVaKISvivqEsY2UfoBNhu7OTPg",
+  authDomain: "floorballlive.firebaseapp.com",
+  projectId: "floorballlive",
+  storageBucket: "floorballlive.appspot.com",
+  messagingSenderId: "935077431603",
+  appId: "1:935077431603:web:cbfe8e4d3f56f6d3165646"
 };
 
 // Initialize Firebase
